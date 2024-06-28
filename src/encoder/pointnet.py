143131266267e8294325cb09ae21aaabac2ecb5e
Robin Oberson <robin.oberson27@gmail.com>
2024-06-28 07:32:01 +0200

    no torch scatter

--- src/encoder/pointnet.py
+++ src/encoder/pointnet.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 from src.layers import ResnetBlockFC
-from torch_scatter import scatter_mean, scatter_max
 from src.common import coordinate2index, normalize_3d_coordinate, \
     map2local, positional_encoding
 from src.encoder.unet3d_noskipconnection import UNet3D
@@ -9,102 +8,6 @@ from src.encoder.unet3d_noskipconnection_latent import UNet3D_noskipconnection_l
 import math
 import time
 
-
-class LocalPoolPointnet(nn.Module):
-    ''' PointNet-based encoder network with ResNet blocks for each point.
-        Number of input points are fixed.
-
-    Args:
-        c_dim (int): dimension of latent code c
-        dim (int): input points dimension
-        hidden_dim (int): hidden dimension of the network
-        scatter_type (str): feature aggregation when doing local pooling
-        unet3d (bool): weather to use 3D U-Net
-        unet3d_kwargs (str): 3D U-Net parameters
-        grid_resolution (int): defined resolution for grid feature
-        plane_type (str): feature type, 'grid' - 3D grid volume
-        padding (float): conventional padding paramter of ONet for unit cube, so [-0.5, 0.5] -> [-0.55, 0.55]
-        n_blocks (int): number of blocks ResNetBlockFC layers
-    '''
-
-    def __init__(self, c_dim=128, dim=3, hidden_dim=128, scatter_type='max',
-                 unet3d=False, unet3d_kwargs=None,
-                 grid_resolution=None, plane_type='grid', padding=0.1, n_blocks=5,
-                 pos_encoding=True, unit_size=0.1):
-        super().__init__()
-        self.c_dim = c_dim
-
-        if pos_encoding == True:
-            dim = 60
-        self.fc_pos = nn.Linear(dim, 2 * hidden_dim)
-        self.blocks = nn.ModuleList([
-            ResnetBlockFC(2 * hidden_dim, hidden_dim) for i in range(n_blocks)
-        ])
-        self.fc_c = nn.Linear(hidden_dim, c_dim)
-        self.actvn = nn.ReLU()
-        self.hidden_dim = hidden_dim
-
-        if unet3d:
-            self.unet3d = UNet3D_noskipconnection_latent(**unet3d_kwargs)
-        else:
-            self.unet3d = None
-
-        self.reso_grid = grid_resolution
-        self.plane_type = plane_type
-        self.padding = padding
-
-        if scatter_type == 'max':
-            self.scatter = scatter_max
-        elif scatter_type == 'mean':
-            self.scatter = scatter_mean
-        else:
-            raise ValueError('incorrect scatter type')
-
-        self.pos_encoding = pos_encoding
-        if pos_encoding:
-            self.pe = positional_encoding()
-
-    def generate_grid_features(self, p, c):
-        p_nor = normalize_3d_coordinate(p.clone(), padding=self.padding)
-        index = coordinate2index(p_nor, self.reso_grid, coord_type='3d')
-        # scatter grid features from points
-        fea_grid = c.new_zeros(p.size(0), self.c_dim, self.reso_grid ** 3)
-        c = c.permute(0, 2, 1)
-        fea_grid = scatter_mean(c, index, out=fea_grid)  # B x C x reso^3
-        fea_grid = fea_grid.reshape(p.size(0), self.c_dim, self.reso_grid, self.reso_grid,
-                                    self.reso_grid)  # sparce matrix (B x 512 x reso x reso)
-
-        if self.unet3d is not None:
-            fea_grid, latent_code = self.unet3d(fea_grid)
-            return fea_grid, latent_code
-        else:
-            return fea_grid, None
-
-    def pool_local(self, xy, index, c):
-        bs, fea_dim = c.size(0), c.size(2)
-        keys = xy.keys()
-
-        c_out = 0
-        for key in keys:
-            # scatter plane features from points
-            if key == 'grid':
-                fea = self.scatter(c.permute(0, 2, 1), index[key], dim_size=self.reso_grid ** 3)
-
-            if self.scatter == scatter_max:
-                fea = fea[0]
-            # gather feature back to points
-            fea = fea.gather(dim=2, index=index[key].expand(-1, fea_dim, -1))
-            c_out += fea
-        return c_out.permute(0, 2, 1)
-
-    def forward(self, p):
-        
-        fea = {}
-
-        if 'grid' in self.plane_type:
-            fea['grid'], latent_code = self.generate_grid_features(p, c)
-
-        return fea, net, c, latent_code
 
 class PatchLocalPoolPointnetLatent(nn.Module):
     ''' PointNet-based encoder network with ResNet blocks.
@@ -115,7 +18,6 @@ class PatchLocalPoolPointnetLatent(nn.Module):
         c_dim (int): dimension of latent code c
         dim (int): input points dimension
         hidden_dim (int): hidden dimension of the network
-        scatter_type (str): feature aggregation when doing local pooling
         unet3d (bool): weather to use 3D U-Net
         unet3d_kwargs (str): 3D U-Net parameters
         grid_resolution (int): defined resolution for grid feature
